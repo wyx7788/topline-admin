573b03fe90f24275146b2bb5b86253e29141763e
wang <1348958548@qq.com>
2019-06-23 10:24:29 +0800

    gt.js上传

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -14,7 +14,7 @@
             <el-input v-model="ruleForm.code"></el-input>
           </el-col>
           <el-col :span="8" :offset="2">
-            <el-button>获取验证码</el-button>
+            <el-button @click="handleSendCode">获取验证码</el-button>
           </el-col>
         </el-form-item>
         <el-form-item>
@@ -27,14 +27,18 @@
 </template>
 
 <script>
+import axios from 'axios'
+import '@/vendor/gt.js'
+
 export default {
   name: 'AppLogin',
   data () {
     return {
       ruleForm: {
-        mobile: '',
+        mobile: '17635158848',
         code: ''
       },
+      captchaObj: null,
       rules: {
         mobile: [
           { required: true, message: '请输入手机号码', trigger: 'blur' },
@@ -48,6 +52,53 @@ export default {
     }
   },
   methods: {
+    handleSendCode () {
+      const mobile = this.ruleForm.mobile
+      console.log(this.ruleForm)
+      if (this.captchaObj) {
+        return this.captchaObj.verify()
+      }
+      axios({
+        method: 'GET',
+        url: `http://ttapi.research.itcast.cn/mp/v1_0/captchas/${mobile}`
+      }).then(res => {
+        console.log(res.data)
+        const data = res.data.data
+        window.initGeetest({
+          // 以下配置参数来自服务端 SDK
+          gt: data.gt,
+          challenge: data.challenge,
+          offline: !data.success,
+          new_captcha: true,
+          product: 'bind'
+        },
+        (captchaObj) => {
+          this.captchaObj = captchaObj
+          console.log(captchaObj)
+          // 这里可以调用验证实例 captchaObj 的实例方法
+          captchaObj.onReady(function () {
+            captchaObj.verify()
+            // 验证码ready之后才能调用verify方法显示验证码
+          }).onSuccess(function () {
+            // your code
+            var result = captchaObj.getValidate()
+            // ajax 伪代码，进行二次验证
+            axios({
+              url: `http://ttapi.research.itcast.cn/mp/v1_0/sms/codes/${mobile}`,
+              geetest_challenge: result.geetest_challenge,
+              geetest_validate: result.geetest_validate,
+              geetest_seccode: result.geetest_seccode
+            }).then(res => {
+              close.log(res.data)
+              // 其他服务端需要的数据，比如登录时的用户名和密码
+              // 根据服务端二次验证的结果进行跳转等操作
+            })
+          }).onError(function () {
+            // your code
+          })
+        })
+      })
+    },
     submitForm (formName) {
       this.$refs[formName].validate((valid) => {
         if (valid) {
